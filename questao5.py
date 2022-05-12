ce530d1b8102caf208b4de72f1085683e3ec8589
Handerson Ramos Barbosa <handerson.cge1@live.com>
2022-05-11 22:32:26 -0300

    Resposta de questão
    Elaborar uma rotina responsável por recebe dois conjunto de palavras separadas por virgula e armazene os valores existentes em ambos os conjuntos.No final imprimir os valores obtidos.

---
+++ questao5.py
@@ -0,0 +1,8 @@
+conjuntoA=["agua","terra","sol","lua","bola","chuva"]
+conjuntoB=["agua","marte","terremoto","sol","lua","bola","CHUVA"]
+
+for a in conjuntoA:
+    for b in conjuntoB:
+        if (a==b):
+            print(a)
+            break
